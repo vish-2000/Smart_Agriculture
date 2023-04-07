6b5507e0a8c7ca32bd688101b9461b6e4cb25121
Vishesh Agarwal <54229315+vish-2000@users.noreply.github.com>
2023-04-07 20:00:10 +0530

    Setting Configuration Details
    Added Device ID and API Key for two Bolt WiFi Module.
    Added Telegram Channel ID and Telegram Bot Token.

---
+++ VMCode/Configuration.py
@@ -0,0 +1,11 @@
+#Vishesh
+bolt_api_key1 = "f54fe078-bbec-46d3-b4ee-3a73a36363c2"
+device_id1 = "BOLT3851530"
+
+#Sarani
+bolt_api_key2 = "179a15fa-aed4-4279-a262-eeba7798472d"
+device_id2 = "BOLT14854843"
+
+#Telegram Bot
+telegram_chat_id = "@smart_agriculture_sit"
+telegram_bot_id = "bot6227789889:AAEM2RxMdAB4oaIddZ2_yfTIzXmktYJXSNc"
